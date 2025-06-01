6a1e1cfdf3bfb7af9ee75ae4ca15f3544f58db70
Prabhjot Singh <sandhu7707@gmail.com>
2025-06-02 01:14:32 +0530

    removed duplicate state 'result', useReducer for better readability

--- frontend/searchbar-lazy-loading/src/App.js
+++ frontend/searchbar-lazy-loading/src/App.js
@@ -1,130 +1,153 @@
 import './App.css';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
+
+const LIMIT = 5;
+const DEBOUNCE_DELAY = 500;
+
+const initialState = {
+  searchStrVal: '',
+  students: [],
+  selectedStudent: null,
+}
+
+function reducers(state, action) {
+
+  switch (action.type) {
+    case 'reset-results': {
+      return {
+        ...state,
+        results: []
+      }
+    }
+    case 'fetch-success': {
+      let data = action.data;
+      let s = state.students;
+      let filteredResults = [];
+      for (let item of data) {
+        let eligible = true;
+        for (let student of s) {
+          if (item.rollNumber === student.rollNumber) {
+            eligible = false;
+            break;
+          }
+        }
+        if (eligible) {
+          filteredResults.push(item)
+        }
+      }
+
+      if (filteredResults.length > 0) {
+        console.log(filteredResults.length, " new students loaded from fetched results")
+        return {
+          ...state,
+          students: s.concat(filteredResults).sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
+        }
+      }
+      else {
+        console.log("no new results fetched")
+        return state
+      }
+    }
+    case 'select-result': {
+      let filteredStudents = state.students.filter(it => it.rollNumber === parseInt(action.rollNumber))
+      return {
+        ...state,
+        selectedStudent: filteredStudents[0]
+      }
+    }
+    case 'search-str-input': {
+      let data = action.data;
+      return {
+        ...state,
+        searchStrVal: data
+      }
+    }
+    case 'fetch-started': {
+      return {
+        ...state,
+        fetchTrigger: false
+      }
+    }
+    default: {
+      console.error('reducer action not defined!')
+      return state;
+    }
+  }
+}
 
 function App() {
-  const limit = 5;
-  const debounceDelay = 1000;
-  // const [triggerSwitch, setTriggerSwitch] = useState(false)
-  const [searchStrVal, setSearchStrVal] = useState('')
-  const [students, setStudents] = useState([])
-  const [results, setResults] = useState([])
-  const [selectedStudent, setSelectedStudent] = useState()
-  const debounce = useRef({clear: null, time: null});
 
-  const searchRef = useRef(null);
-  
-  const fetchNames = useCallback((nameStr, newLimit) => {
-    console.log(`fetching for ${nameStr} with limit ${newLimit}`)
-    fetch(`http://localhost:8080/search/name/${nameStr}/${newLimit}`)
-    .then((data) => data.json())
-    .then((data) => {
+  const [state, dispatch] = useReducer(reducers, initialState)
+  let searchStrVal = state.searchStrVal;
+  const selectedStudent = state.selectedStudent;
+  const students = state.students;
 
-        setStudents((s) => {
-          let filteredResults = [];
-            for(let item of data){
-              let eligible = true;
-              for(let student of s){
-                if(item.rollNumber === student.rollNumber){
-                  eligible = false;
-                  break;
-                }
-              }
-              if(eligible){
-                filteredResults.push(item)
-              }
-            }
-
-            if(filteredResults.length > 0){
-              return s.concat(filteredResults).sort((a,b) => a.name<b.name ? -1 : a.name>b.name ? 1 : 0)
-            }
-            else{
-              console.log("no new results")
-              return s
-            }
-        });
-    })
-  }, [])
-
-  const searchStr = useCallback((nameStr) => {
-      nameStr = nameStr.toLowerCase()
-      let results = []
-      for(let i0=0; i0<students.length && results.length < limit; i0++){
-          if(students[i0].name.toLowerCase().startsWith(nameStr)) {
-              results.push(students[i0])
-          }
+  searchStrVal = searchStrVal.toLowerCase()
+  let results = []
+  if(searchStrVal.length >= 3){
+    for (let i0 = 0; i0 < students.length && results.length < LIMIT; i0++) {
+      if (students[i0].name.toLowerCase().startsWith(searchStrVal)) {
+        results.push(students[i0])
       }
-        
-      let newLimit = limit - results.length;
-        let de = debounce.current;
-        if(newLimit > 0){
-          let delay = debounceDelay;
-          if(de.time){
-            let elapsed =  document.timeline.currentTime - de.time
-            console.log(elapsed)
-            console.log(de.delay)
-            if(elapsed < de.delay){
-              console.log("cleared timeout for id: ", de.clear)
-              clearTimeout(de.clear);
-              delay = de.delay - elapsed;
-            }
-            else{
-              console.log("not cleared for ,", de.clear)
-            }
-          }
-          else{
-            console.log("no debounce.time for", de.clear)
-          }
-          let clear = setTimeout(() => fetchNames(nameStr, limit), delay);
-          debounce.current = {clear: clear, time: document.timeline.currentTime, delay: delay}
-        }
-
-      for(let i0=0; i0<students.length && results.length< limit; i0++){
-          if(students[i0].name.toLowerCase().includes(nameStr) && !results.find(it => it.name === students[i0].name)){
-              results.push(students[i0])
-          }
-      }
-
-      return results;
-  }, [fetchNames, students])
-
-  useEffect(() => {
-
-    if(searchStrVal.length >= 3){
-      let newResults = searchStr(searchStrVal)
-      setResults(newResults)
-    }
-    else{
-      setResults([])
     }
 
-  }, [searchStrVal, searchStr])
-
-  useEffect(()=> {
-    console.log(students)
-    console.log("results updated, new results: ", results)
-  }, [results, students])
-
-  function handleSelect(rollNumber){
-    console.log(rollNumber)
-    let selectedStudent = students.find(it => it.rollNumber === parseInt(rollNumber))
-    setSelectedStudent(selectedStudent)
-    console.log("selected student, ", selectedStudent)
+    for (let i0 = 0; i0 < students.length && results.length < LIMIT; i0++) {
+      if (students[i0].name.toLowerCase().includes(searchStrVal) && !results.find(it => it.name === students[i0].name)) {
+        results.push(students[i0])
+      }
+    }
   }
 
+  const debounce = useRef({ clear: null, time: null });
+  useEffect(() => {
+    if (searchStrVal.length >= 3 && results.length < LIMIT) {
+      const fetchNames = (nameStr) => {
+        fetch(`http://localhost:8080/search/name/${nameStr}/${LIMIT}`)
+        .then((data) => data.json())
+        .then((data) => {
+          dispatch({ type: 'fetch-success', data: data })
+        })
+      
+      }
+      let de = debounce.current;
+      let delay = DEBOUNCE_DELAY;
+      if (de.time) {
+        let elapsed = document.timeline.currentTime - de.time
+        if (elapsed < de.delay) {
+          clearTimeout(de.clear);
+          delay = de.delay - elapsed;
+        }
+      }
+      let nameStr = searchStrVal.toLowerCase()
+      let clear = setTimeout(() => fetchNames(nameStr), delay);
+      dispatch({type: 'fetch-started'})
+      debounce.current = { clear: clear, time: document.timeline.currentTime, delay: delay }
+    }
+    else if (searchStrVal.length < 3) {
+      dispatch({ type: 'reset-results' })
+    }
+
+  }, [searchStrVal])
+
+  function handleSelect(rollNumber) {
+    dispatch({ type: 'select-result', rollNumber: rollNumber })
+  }
+
+  const searchRef = useRef(null);
   window.onclick = (e) => {
-    if(e.target !== searchRef.current){
-      setSearchStrVal('')
+    if (e.target !== searchRef.current) {
+      dispatch({ type: 'search-str-input', data: '' })
     }
   }
 
   return (
     <div className="App">
       <div className='search-container'>
-        <input ref={searchRef} className="searchbar" type='search' style={{width: '100%'}} value={searchStrVal} onChange={(e)=> setSearchStrVal(e.target.value)}/>
-        <SearchResults results={results} handleSelect={handleSelect} searchStrVal={searchStrVal}/>
+        <input ref={searchRef} className="searchbar" type='search' style={{ width: '100%' }} value={searchStrVal} onChange={(e) => dispatch({ type: 'search-str-input', data: e.target.value })} />
+        <SearchResults results={results} handleSelect={handleSelect} searchStrVal={searchStrVal} />
       </div>
       <div className='selected-result'>
-        {selectedStudent && 
+        {selectedStudent &&
           <section>
             <p className='selected-result-row'>Roll Number: {selectedStudent.rollNumber}</p>
             <p className='selected-result-row'>Name: {selectedStudent.name}</p>
@@ -136,31 +159,31 @@ function App() {
   );
 }
 
-function SearchResults({results, handleSelect, searchStrVal}){
-  
+function SearchResults({ results, handleSelect, searchStrVal }) {
+
   const getResultString = (result) => {
-    if(!result){
+    if (!result) {
       return
     }
     let name = result.name;
     let regex = new RegExp(searchStrVal, 'i')
     let match = regex.exec(name)
-    if(!match){
+    if (!match) {
       return <>{name}, Roll Number: {result.rollNumber}</>
     }
     let nameBefore = name.slice(0, match.index)
-    let nameAfter = name.slice(match.index+match[0].length, name.length)
+    let nameAfter = name.slice(match.index + match[0].length, name.length)
     return <>{nameBefore}<b>{match[0]}</b>{nameAfter}, Roll Number: {result.rollNumber}</>;
   }
 
-  return(
-        <div className="search-results-container">
-          <div className='search-results'>
-            {results.map((result) => 
-              <div className="search-result" key={result.rollNumber} onClick={() => handleSelect(result.rollNumber)}>{getResultString(result)}</div>
-            )}
-          </div>
-        </div>    
+  return (
+    <div className="search-results-container">
+      <div className='search-results'>
+        {results.map((result) =>
+          <div className="search-result" key={result.rollNumber} onClick={() => handleSelect(result.rollNumber)}>{getResultString(result)}</div>
+        )}
+      </div>
+    </div>
   )
 }
 
